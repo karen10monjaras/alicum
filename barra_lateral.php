99544ff77b9c153fb6d3042b17405fc2015dfce0
Arturo -sh <salashernandezarturo1512@gmail.com>
2023-10-27 17:43:03 -0600

    Avance de la sección ventas, ya se puede visualizar el agregado de productos al carrito de compras y se actualiza el total a pagar

--- barra_lateral.php
+++ barra_lateral.php
@@ -1,25 +1,25 @@
-      <div class="navbar-content">
-				<ul class="pc-navbar">
-          <li class="pc-item">
-						<a href="#" class="pc-link "><span class="pc-micon"><i class="fa fas fa-home"></i></span><span class="pc-mtext">Inicio</span></a>
-					</li>
-					<li class="pc-item">
-						<a href="#" class="pc-link "><span class="pc-micon"><i class="fa fas fa-dollar-sign"></i></span><span class="pc-mtext">Ventas</span></a>
-					</li>
-					<li class="pc-item">
-						<a href="#" class="pc-link "><span class="pc-micon"><i class="fa fas fa-shopping-bag"></i></span><span class="pc-mtext">Compras</span></a>
-					</li>
-					<li class="pc-item">
-						<a href="#" class="pc-link "><span class="pc-micon"><i class="fa fas fa-warehouse"></i></span><span class="pc-mtext">Almacén</span></a>
-					</li>
-          <li class="pc-item">
-						<a href="#" class="pc-link "><span class="pc-micon"><i class="fa fas fa-address-book"></i></span><span class="pc-mtext">Clientes</span></a>
-					</li>
-          <li class="pc-item">
-						<a href="#" class="pc-link "><span class="pc-micon"><i class="fa fas fa-truck"></i></span><span class="pc-mtext">Proveedores</span></a>
-					</li>
-          <li class="pc-item">
-						<a href="#" class="pc-link "><span class="pc-micon"><i class="fa fas fa-users"></i></span><span class="pc-mtext">Usuarios</span></a>
-					</li>
-				</ul>
-			</div>
+<div class="navbar-content">
+	<ul class="pc-navbar">
+    	<li class="pc-item">
+			<a href="?modulo=inicio" class="pc-link "><span class="pc-micon"><i class="fa fas fa-home"></i></span><span class="pc-mtext">Inicio</span></a>
+		</li>
+		<li class="pc-item">
+			<a href="?modulo=ventas" class="pc-link "><span class="pc-micon"><i class="fa fas fa-dollar-sign"></i></span><span class="pc-mtext">Ventas</span></a>
+		</li>
+		<li class="pc-item">
+			<a href="?modulo=compras" class="pc-link "><span class="pc-micon"><i class="fa fas fa-shopping-bag"></i></span><span class="pc-mtext">Compras</span></a>
+		</li>
+		<li class="pc-item">
+			<a href="?modulo=almacen" class="pc-link "><span class="pc-micon"><i class="fa fas fa-warehouse"></i></span><span class="pc-mtext">Almacén</span></a>
+		</li>
+    	<li class="pc-item">
+			<a href="?modulo=clientes" class="pc-link "><span class="pc-micon"><i class="fa fas fa-address-book"></i></span><span class="pc-mtext">Clientes</span></a>
+		</li>
+    	<li class="pc-item">
+			<a href="?modulo=proveedores" class="pc-link "><span class="pc-micon"><i class="fa fas fa-truck"></i></span><span class="pc-mtext">Proveedores</span></a>
+		</li>
+    	<li class="pc-item">
+			<a href="?modulo=usuarios" class="pc-link "><span class="pc-micon"><i class="fa fas fa-users"></i></span><span class="pc-mtext">Usuarios</span></a>
+		</li>
+	</ul>
+</div>
--- index.php
+++ index.php
@@ -141,10 +141,107 @@ if (!isset($_SESSION['id_usuario'])) {
     <!-- <script src="assets/js/uikit.min.js"></script> -->
 
 <!-- Apex Chart -->
-<script src="assets/js/plugins/apexcharts.min.js"></script>
+<!-- <script src="assets/js/plugins/apexcharts.min.js"></script> -->
 
 <!-- custom-chart js -->
-<script src="assets/js/pages/dashboard-sale.js"></script>
-</body>
+<!-- <script src="assets/js/pages/dashboard-sale.js"></script> -->
 
+<!-- jquery js -->
+<script src="assets/js/plugins/jquery/jquery.min.js"></script>
+
+<script>
+$(document).ready(function() {
+    id_productos = [];
+  
+    $('#key').on('keyup', function() {
+      var key = $(this).val();		
+      var dataString = 'key='+key;
+  
+      $.ajax({
+        type: "POST",
+        url: "modulos/inicio/search.php",
+        data: dataString,
+        success: function(response) {
+          $('#suggestions').fadeIn(1000).html(response);
+          $('.suggest-element').on('click', function(){
+            var id = $(this).attr('id');
+            var precio = $(this).attr('precio');
+            var producto = $(this).text();
+  
+			var existingRow = $('#tbl-productos tr[data-id="' + id + '"]');
+            if (existingRow.length > 0) {
+                var cantidadInput = existingRow.find('input[type="number"]');
+                var cantidad = parseInt(cantidadInput.val());
+                cantidadInput.val(cantidad + 1);
+            } else {
+                template = `
+                <tr data-id="${id}">
+                    <td>8923716472</td>
+                    <td>${producto}</td>
+                    <td>$ ${precio}</td>
+                    <td>
+                        <div class="form-outline">
+                            <input type="number" class="form-control" value="1"/>
+                        </div>
+                    </td>
+                    <td>
+						<button id="${id}" class="btn btn-sm btn-outline-danger delete-product" data-id="${id}">
+							<i class="fa fas fa-trash"></i>
+						</button>
+					</td>
+                </tr>`;
+				$('#tbl-productos').append(template);
+            }
+            id_productos.push(id);
+
+			calcularTotal();
+            $('#suggestions').fadeOut(1000);
+            $("#key").val("");
+            return false;
+        });
+        }
+      });
+    });
+
+	// Manejador de eventos para eliminar productos
+    $('#tbl-productos').on('click', '.delete-product', function () {
+        var id = $(this).data('id');
+
+        // Eliminar el producto de la tabla
+        $(this).closest('tr').remove();
+
+        // Eliminar el producto del arreglo
+        var index = id_productos.indexOf(id);
+        if (index !== -1) {
+            id_productos.splice(index, 1);
+        }
+
+        calcularTotal(); // Llama a la función para recalcular el total
+    });
+
+    // Manejador de eventos para cambiar la cantidad de productos
+    // $('#tbl-productos').on('change', 'input[type="number"]', function () {
+    //     // ... (tu código para cambiar la cantidad de productos)
+    //     calcularTotal(); // Llama a la función para recalcular el total
+    // });
+
+    // Función para calcular el total
+    function calcularTotal() {
+        var total = 0;
+        $('#tbl-productos tr').each(function () {
+            var cantidad = parseInt($(this).find('input[type="number"]').val());
+            var precio = parseFloat($(this).find('td:eq(2)').text().replace('$', ''));
+            var subtotal = cantidad * precio;
+            total += subtotal;
+        });
+		console.log(total)
+
+        // Actualiza el elemento donde deseas mostrar el total
+        $('#total-pagar').text(total.toFixed(2));
+    }
+
+  });
+</script>
+
+</body>
 </html>
