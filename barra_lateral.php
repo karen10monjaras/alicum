4e92a1fdb12c4cb185f08bfef39a54585f724d97
Arturo -sh <salashernandezarturo1512@gmail.com>
2023-10-29 19:34:22 -0600

    Se corrigió la barra lateral, ahora se muestra que sección del sistema se encuentra activa

--- barra_lateral.php
+++ barra_lateral.php
@@ -1,25 +1,57 @@
-<div class="navbar-content">
-	<ul class="pc-navbar">
-    	<li class="pc-item">
-			<a href="?modulo=inicio" class="pc-link "><span class="pc-micon"><i class="fa fas fa-dollar-sign"></i></span><span class="pc-mtext">Generar venta</span></a>
+<?php
+
+$inicio = '';
+$ventas = '';
+$compras = '';
+$almacen = '';
+$clientes = '';
+$proveedores = '';
+$usuarios = '';
+
+if (!empty($_GET) && isset($_GET['modulo'])) {
+    switch($_GET['modulo']){
+      case 'inicio':
+        $inicio = 'active'; break;
+      case 'ventas':
+        $ventas = 'active'; break;
+      case 'compras':
+        $compras = 'active'; break;
+      case 'almacen':
+        $almacen = 'active'; break;
+      case 'clientes':
+        $clientes = 'active'; break;
+	  case 'proveedores':
+        $proveedores = 'active'; break;
+	  case 'usuarios':
+        $usuarios = 'active'; break;
+    }
+}
+
+echo "
+<div class='navbar-content'>
+	<ul class='pc-navbar'>
+    	<li class='pc-item $inicio'>
+			<a href='?modulo=inicio' class='pc-link'><span class='pc-micon'><i class='fa fas fa-dollar-sign'></i></span><span class='pc-mtext'>Generar venta</span></a>
 		</li>
-		<li class="pc-item">
-			<a href="?modulo=ventas" class="pc-link "><span class="pc-micon"><i class="fa fas fa-history"></i></span><span class="pc-mtext">Historial ventas</span></a>
+		<li class='pc-item $ventas'>
+			<a href='?modulo=ventas' class='pc-link'><span class='pc-micon'><i class='fa fas fa-history'></i></span><span class='pc-mtext'>Historial ventas</span></a>
 		</li>
-		<li class="pc-item">
-			<a href="?modulo=compras" class="pc-link "><span class="pc-micon"><i class="fa fas fa-shopping-bag"></i></span><span class="pc-mtext">Compras</span></a>
+		<li class='pc-item $compras'>
+			<a href='?modulo=compras' class='pc-link'><span class='pc-micon'><i class='fa fas fa-shopping-bag'></i></span><span class='pc-mtext'>Compras</span></a>
 		</li>
-		<li class="pc-item">
-			<a href="?modulo=almacen" class="pc-link "><span class="pc-micon"><i class="fa fas fa-warehouse"></i></span><span class="pc-mtext">Almacén</span></a>
+		<li class='pc-item $almacen'>
+			<a href='?modulo=almacen' class='pc-link'><span class='pc-micon'><i class='fa fas fa-warehouse'></i></span><span class='pc-mtext'>Almacén</span></a>
 		</li>
-    	<li class="pc-item">
-			<a href="?modulo=clientes" class="pc-link "><span class="pc-micon"><i class="fa fas fa-address-book"></i></span><span class="pc-mtext">Clientes</span></a>
+    	<li class='pc-item $clientes'>
+			<a href='?modulo=clientes' class='pc-link'><span class='pc-micon'><i class='fa fas fa-address-book'></i></span><span class='pc-mtext'>Clientes</span></a>
 		</li>
-    	<li class="pc-item">
-			<a href="?modulo=proveedores" class="pc-link "><span class="pc-micon"><i class="fa fas fa-truck"></i></span><span class="pc-mtext">Proveedores</span></a>
+    	<li class='pc-item $proveedores'>
+			<a href='?modulo=proveedores' class='pc-link'><span class='pc-micon'><i class='fa fas fa-truck'></i></span><span class='pc-mtext'>Proveedores</span></a>
 		</li>
-    	<li class="pc-item">
-			<a href="?modulo=usuarios" class="pc-link "><span class="pc-micon"><i class="fa fas fa-users"></i></span><span class="pc-mtext">Usuarios</span></a>
+    	<li class='pc-item $usuarios'>
+			<a href='?modulo=usuarios' class='pc-link'><span class='pc-micon'><i class='fa fas fa-users'></i></span><span class='pc-mtext'>Usuarios</span></a>
 		</li>
 	</ul>
-</div>
+</div>";
+
+?>
